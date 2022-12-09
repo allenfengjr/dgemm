05ea1611987b4fdb0a1c3678591d37e9ef415795
Hao Feng <allenfengjr@gmail.com>
2022-12-09 11:22:57 -0500

    Update dgemm.cpp

--- dgemm.cpp
+++ dgemm.cpp
@@ -13,7 +13,7 @@
 #include "Vector.hpp"
 /*
  * args usage:
- * argv[1],[2],[3] matrix file name for A(m*l), B(l*n), C(m*n)
+ * argv[1],[2],[3] matrix block size m, l, n
  * argv[4] OpenMP #Thread per task -- default 1
  * argv[5] the value of alpha -- default 1
  * argv[6] the value of beta -- default 0
@@ -37,45 +37,35 @@ int main(int argc, char* argv[]){
         exit(-1);
     }
     int k = (int) sqrt(n_ranks);
-    int m_size, n_size, l_size;
+    int m_size= atoi(argv[1]);
+    int l_size= atoi(argv[2]);
+    int n_size= atoi(argv[3]);
     if(my_rank==0){
-        MMIOReader A(argv[1]);
-        MMIOReader B(argv[2]);
-        MMIOReader C(argv[3]);
-        //check shape of three matrix
-        if(A.n_rows!=C.n_rows||B.n_columns!=C.n_columns||A.n_columns!=B.n_rows){
-            std::cout<<"the shapes of matrix can not match"<<std::endl;
-            MPI_Finalize();
-            exit(-1);
-        }
-        m_size = A.n_rows;
-        l_size = A.n_columns;
-        n_size = B.n_columns;
-        MPI_Bcast(&m_size,1,MPI_INT,0,MPI_COMM_WORLD);
-        MPI_Bcast(&n_size,1,MPI_INT,0,MPI_COMM_WORLD);
-        MPI_Bcast(&l_size,1,MPI_INT,0,MPI_COMM_WORLD);
+        //generate three matrix, row-major
+        std::vector<double> A(m_size*l_size*n_ranks,1.0);
+        std::vector<double> B(l_size*n_size*n_ranks,1.0);
+        std::vector<double> C(m_size*n_size*n_ranks,1.0);
     }
-    std::vector<int> m_spilt(k);
-    std::vector<int> n_spilt(k);
-    std::vector<int> l_spilt(k);
-    for (int i = 0; i < k; ++i) {
-        m_spilt[i] = (i<(int)m_size%k)? ceil(m_size/k): floor(m_size/k);
-        n_spilt[i] = (i<(int)n_size%k)? ceil(n_size/k): floor(n_size/k);
-        l_spilt[i] = (i<(int)l_size%k)? ceil(l_size/k): floor(l_size/k);
-    }
-    //create the largest space, if
-    std::vector<int> sub_A(m_spilt[0]*l_spilt[0]);
-    std::vector<int> sub_B(l_spilt[0]*n_spilt[0]);
-    std::vector<int> sub_C(m_spilt[0]*n_spilt[0]);
+
+    //create the buffer
+    std::vector<int> sub_A(m_size*l_size);
+    std::vector<int> sub_B(l_size*n_size);
+    std::vector<int> sub_C(m_size*n_size);
     if(my_rank==0){
         //re-order the data for the following MPI_Scatter
         std::vector<double> A_v(m_size*l_size);
         std::vector<double> B_v(l_size*n_size);
         std::vector<double> C_v(m_size*n_size);
-        for (int i = 0; i < n_ranks; ++i) {
+        int A_count = 0, B_count = 0, C_count = 0;
+        for (int p = 0; p < n_ranks; ++p) {
             //assign every sub-matrix, in my program, because all nodes except root node do not have sub-matrix
             //I just sent them the right sub-matrix after first shift. So they do not need to shift 1-k steps.
-            
+            int A_start = l_size *
+            int B_start = n_size * l_start[p/k] + l_spilt[p/k]*n_start[p%k];
+            for(int i = 0; i < m_spilt[p%k];++i){
+                //copy one row
+                std::copy(A.begin()+(m_start[p%]))
+            }
 
         }
 
@@ -86,13 +76,24 @@ int main(int argc, char* argv[]){
 
     //2. Permute Sub-Matrix
     MPI_Scatterv(B_v);
-
+    int nbrs[4],dims[2]={k,k},periods[2]={1,1},reorder=0,coords[2];
+    MPI_Comm cartcomm;
+    MPI_Cart_create(MPI_COMM_WORLD,2,dims,periods,reorder,&cartcomm);
+    MPI_Cart_coords(cartcomm,my_rank,2,coords);
+    MPI_Cart_shift(cartcomm,0,1,&nbrs[0],&nbrs[1]);
+    MPI_Cart_shift(cartcomm,1,1,&nbrs[2],&nbrs[3]);
+    MPI_Sendrecv_replace(sub_A.data(),3232,MPI_DOUBLE,nbrs[2],3,nbrs[3],3,MPI_COMM_WORLD,&status);
+    MPI_Sendrecv_replace(sub_B.data(),1323,MPI_DOUBLE,nbrs[0],4,nbrs[1],4,MPI_COMM_WORLD,&status);
 
     //3. Do k-times multiplication and movement, use OpenMP at the multiplication part.
     // multiplication
-    for(int i = 0 ; i < ; ++i){
-        for (int j = 0; j < ; ++j) {
-
+    for (int p = 0; p < k; ++p) {
+        for (int j = 0; j < n_size; ++j) {
+            for (int l = 0; l < l_size; ++l) {
+                for (int i = 0; i < m_size; ++i) {
+                    sub_C[i+j*m_size] += alpha * sub_A[i*l_size+l] * sub_B[l*n_size+j];
+                }
+            }
         }
     }
     //
